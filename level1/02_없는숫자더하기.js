fbfb64ce929c3160ad2c910594991bc0c16caaca
jinsun <yks9013@naver.com>
2021-11-12 10:09:22 +0900

    모범답안추가

--- level1/02_없는숫자더하기.js
+++ level1/02_없는숫자더하기.js
@@ -12,7 +12,16 @@ function solution(numbers) {
     return result;
 }
 
-// 모범답안
+// 모범답안1
 function solution(numbers) {
     return 45 - numbers.reduce((cur, acc) => cur + acc);
+}
+
+// 모범답안2
+function solution(numbers) {
+    let answer = 0;
+    for (let i = 0; i <= 9; i++) {
+        if (!numbers.includes(i)) answer += i;
+    }
+    return answer;
 }
