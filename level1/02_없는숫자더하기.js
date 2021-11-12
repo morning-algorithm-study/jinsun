a990e0bff101f922d7a897e501a49aaba2a8cb6c
jinsun <yks9013@naver.com>
2021-11-12 10:02:01 +0900

    없는숫자더하기

---
+++ level1/02_없는숫자더하기.js
@@ -0,0 +1,18 @@
+// 문제 : https://programmers.co.kr/learn/courses/30/lessons/86051
+// 2021.11.12 level1 - 없는 숫자 더하기
+
+let numbers = [1, 2, 3, 4, 6, 7, 8, 0];
+
+// 내 답안
+function solution(numbers) {
+    let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let result = array
+        .filter((num) => !numbers.includes(num))
+        .reduce((a, b) => a + b);
+    return result;
+}
+
+// 모범답안
+function solution(numbers) {
+    return 45 - numbers.reduce((cur, acc) => cur + acc);
+}
