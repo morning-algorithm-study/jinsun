9f8771b2919a52a2b0a08e9f4c6554945dad6a76
jinsunYoon <yks9013@naver.com>
2021-12-08 18:48:21 +0900

    짝수와 홀수

---
+++ level1/16_짝수와홀수.js
@@ -0,0 +1,9 @@
+// 문제 https://programmers.co.kr/learn/courses/30/lessons/12937?language=javascript
+// 2021.12.08 level1 - 짝수와 홀수
+
+// 내 답안
+function solution(num) {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
+
+// 너무 간단한 문제라 모범답안 생략
