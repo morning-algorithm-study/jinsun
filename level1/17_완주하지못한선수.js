e3b2bbd4bb36c934677e9ad24cead67990cc2c45
jinsun <yks9013@naver.com>
2021-12-11 18:10:25 +0900

    동명이인처리하기

--- level1/17_완주하지못한선수.js
+++ level1/17_완주하지못한선수.js
@@ -2,4 +2,8 @@
 // 2021.12.09 level1 - 완주하지 못한 선수
 
 // 내 답안
-function
+function solution(participant, completion) {
+    let result = [...new Set(participant.concat(completion))];
+    console.log(result);
+}
+// 동명이인 처리가 안됨
